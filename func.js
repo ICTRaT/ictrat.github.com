846b30e5edab5c868508732f501ec9190ab64a0b
Tadashi Ogasawara <1bjt2225@mail.tokai-u.jp>
2014-05-21 11:04:13 +0900

    UP SampleContents
    Sample Upload

---
+++ func.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	//初期状態でのロードファイルを指定
+	$('#mainContents').load('home.html');
+
+	//クリックで各コンテンツを切り替え
+	$('.switchButton').click(function(){
+		var loadContents = $(this).attr('title');
+		$('#mainContents').load(loadContents+'.html');
+	});
+
+	//menuクリック時の動作
+	$("#toggle").click(function(){
+		$("#menu").slideToggle();
+		return false;
+	});
+
+	//ウィンドウがスマホサイズ時の表示切替
+	$(window).resize(function(){
+		var win = $(window).width();
+		var p = 480;
+		if(win > p){
+			$("#menu").show();
+		}else {
+			$("#menu").hide();
+		}
+	});
+});
